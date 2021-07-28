da78867c183c42703eeb330b62f1ee73717e7fdf
Zach <fontenotza@gmail.com>
2021-07-28 14:33:46 -0400

    commented driver file

--- WSB_Scraper.py
+++ WSB_Scraper.py
@@ -24,14 +24,46 @@ import sys
 l = Log()
 l.start_log()
 
-# setup Reddit instance
+# Setup Reddit instance
+# TODO: Look into PRAW multithread support
 reddit = praw.Reddit(
      client_id="KmWrNZao9rWwSA",
      client_secret="V5mH25xahLgeUakjH6Y_xRxQ3fmKSA",
      user_agent="My Reddit Scraper 1.0 by fontenotza"
 )
 
+# Parent queue for message passing (not implemented)
 parent_q = []
+
+
+## Thread Label Key ##
+#   SSR: Stream Reader thread
+#   SSW: Stream Writer thread
+#   SH: Hot Post thread
+
+
+# Data file layout:
+#   /Data
+#       /hot
+#           /{sub1}
+#               AAPL_hot_{sub1}.csv
+#               GME_hot_{sub1}.csv
+#               ...
+#           /{sub2}
+#               AAPL_hot_{sub2}.csv
+#               GME_hot_{sub2}.csv
+#               ...
+#            ...
+#       /stream
+#           /{sub1}
+#               AAPL_stream_{sub1}.csv
+#               GME_stream_{sub1}.csv
+#               ...
+#           /{sub2}
+#               AAPL_stream_{sub2}.csv
+#               GME_stream_{sub2}.csv
+#               ...
+#           ...
 
 # /////////////////////////////////////////////////////////////////
 #   Method: stream_scraper_reader
@@ -42,13 +74,16 @@ parent_q = []
 # /////////////////////////////////////////////////////////////////
 def stream_scraper_reader(q, sub, parent_q):
 
+    # Fatal error catching block
     try:
-        sub_name = sub.display_name
-        thread_native_id = _thread.get_native_id()
+        sub_name = sub.display_name                 # bane of sub to scrape
+        thread_native_id = _thread.get_native_id()  # id of thread
+        # Catch issues with logging
         try:
             l.update_log('Stream scraper running on ' + sub_name, 'SSR '+ str(thread_native_id))
         except Exception as e:
             print('error updating log')
+        # Hourly loop to read comments from queue
         while True:
             try:
                 print('SSR ', thread_native_id, '\t| ', end='')
@@ -60,7 +95,9 @@ def stream_scraper_reader(q, sub, parent_q):
                 print('SSR ', thread_native_id, '\t| ', end='')
                 print(str(len(q)) + ' '+  sub_name + ' comments collected')
 
+                # Pops all comments each hour until queue is empty
                 while True:
+                    # Checks if there is a comment to get
                     try:
                         r_comment = q.pop(0)
                     except Exception as e:
@@ -69,12 +106,15 @@ def stream_scraper_reader(q, sub, parent_q):
                     if r_comment is None:
                         break
 
+                    # Get Comment score
                     comments_processed += 1
                     comment = Comment_Info(r_comment.body, -1, r_comment.score)
                     result = comment_score(comment)
 
+                    # Process Tickers scraped
                     try:
                         if result != None:
+                            # Pulls out metion data and comglomerates for each ticker
                             for new_ticker in result:
                                 new = True
                                 for ticker in tickers:
@@ -87,6 +127,7 @@ def stream_scraper_reader(q, sub, parent_q):
                     except Exception as e:
                         l.update_log('Error in processing comments from ' + sub_name + " :" + str(e), 'SSR '+ str(thread_native_id))
 
+                # End Comment processing
 
                 tickers.sort(key = attrgetter('score'), reverse = True)
                 print('SSR ', thread_native_id, '\t| ', end='')
@@ -96,6 +137,7 @@ def stream_scraper_reader(q, sub, parent_q):
 
                 l.update_log(str(comments_processed) + ' stream comments scraped from ' + sub_name, 'SSR '+ str(thread_native_id))
 
+                # Try writing data to file
                 try:
                     storage_manager(tickers, 'stream', sub_name)
                 except Exception as e:
@@ -106,6 +148,7 @@ def stream_scraper_reader(q, sub, parent_q):
                 wait_for_next_hour()
             except Exception as e:
                 l.update_log('Unexpected error while scraping ' + sub_name + " :" + str(e), 'SSR '+ str(thread_native_id))
+        # End hourly loop
     except Exception as e:
         l.update_log('Unexpected fatal error while scraping ' + sub_name + " :" + str(e), 'SSR '+ str(thread_native_id))
 
@@ -119,38 +162,43 @@ def stream_scraper_reader(q, sub, parent_q):
     #           'sub' - the Subreddit being scraped
     # /////////////////////////////////////////////////////////////////
 def scrape_hot_posts(num, sub, parent_q):
-
+    # Fatal error catching block
     try:
-        sub_name = sub.display_name
-        thread_native_id = _thread.get_native_id()
+        sub_name = sub.display_name                 # bane of sub to scrape
+        thread_native_id = _thread.get_native_id()  # id of thread
+        # Catch issues with logging
         try:
             l.update_log('Hot post scraper running on ' + sub_name, 'SH ' + str(thread_native_id))
         except Exception as e:
             print('error updating log')
-
+        # Hourly loop to obtain hot posts
         while True:
+            # Inner loop try block ensures PRAW interrutions are handled
             try:
-                hot_posts = []
+                hot_posts = []  # list of hot posts for {sub_name}
                 print('SH ', thread_native_id, '\t| ', end='')
                 print('Compiling Hottest ' + str(num) + ' ' + sub_name + ' posts')
 
                 start_hour = int(get_index())   # the current hour at start
 
-                posts_retr = False
-                retries = 0
-                max_retries = 10
-                timeout = False
+                # This block tries obtaining hot posts until PRAW responds correctly or timeout is reached
+                posts_retr = False  # have the posts been retrieved?
+                retries = 0         # number of attempts made this hour
+                max_retries = 10    # max attempts per hour
+                timeout = False     # has max attempts been reached?
                 while not posts_retr and not timeout:
+                    # try to obtain posts
                     try:
                         for submission in sub.hot(limit=num):
                             hot_posts.append(submission)
+                        # check if posts have been collected
                         if len(hot_posts) > 0:
                             posts_retr = True
                     except Exception as e:
                         l.update_log('Could not retrieve hot posts from ' + sub_name + ": " + str(e), 'SH ' + str(thread_native_id))
                     if not posts_retr:
                         retries += 1
-                        sleep(5)
+                        sleep(30)
                     if retries >= max_retries:
                         timeout = True
 
@@ -167,6 +215,8 @@ def scrape_hot_posts(num, sub, parent_q):
                         comments = get_post_comments(submission)  # returns tuples (body, depth, score)
                         print('SH ', thread_native_id, '\t| ', end='')
                         print('Scraping post -> ' + submission.title)
+
+                        # Pulls out metion data and comglomerates for each ticker
                         for comment in comments:
                             comments_processed += 1
                             result = comment_score(comment)
@@ -192,6 +242,7 @@ def scrape_hot_posts(num, sub, parent_q):
 
                 l.update_log(str(comments_processed) + ' hot comments scraped from ' + sub_name, 'SH '+ str(thread_native_id))
 
+                # Try writing data to file
                 try:
                     storage_manager(tickers, 'hot', sub_name)
                 except Exception as e:
@@ -220,6 +271,32 @@ def scrape_hot_posts(num, sub, parent_q):
 # /////////////////////////////////////////////////////////////////
 def storage_manager(tickers, set, sub_name):
 
+    # Write each ticker score to appropriate SymbolDirectory
+
+    # Data file layout:
+    #   /Data
+    #       /hot
+    #           /{sub1}
+    #               AAPL_hot_{sub1}.csv
+    #               GME_hot_{sub1}.csv
+    #               ...
+    #           /{sub2}
+    #               AAPL_hot_{sub2}.csv
+    #               GME_hot_{sub2}.csv
+    #               ...
+    #            ...
+    #       /stream
+    #           /{sub1}
+    #               AAPL_stream_{sub1}.csv
+    #               GME_stream_{sub1}.csv
+    #               ...
+    #           /{sub2}
+    #               AAPL_stream_{sub2}.csv
+    #               GME_stream_{sub2}.csv
+    #               ...
+    #           ...
+
+    # process each ticker
     for ticker in tickers:
         file_name = 'Data/' + set + '/' + sub_name + '/' + ticker.symbol + '_data_' + set + '.csv'
         file_path = pathlib.Path(file_name)
@@ -262,11 +339,13 @@ def storage_manager(tickers, set, sub_name):
 # /////////////////////////////////////////////////////////////////
 def stream_scraper_writer(q, stream, sub, parent_q):
 
-    sub_name = sub.display_name
-    thread_native_id = _thread.get_native_id()
-    t_report = _thread.start_new_thread(queue_report, (q, sub_name,))
+    sub_name = sub.display_name                 # bane of sub to scrape
+    thread_native_id = _thread.get_native_id()  # id of thread
+    t_report = _thread.start_new_thread(queue_report, (q, sub_name,))   # Start thread to track queue
     print('SSW ', thread_native_id, '\t| ', end='')
     print(sub_name + ' stream up and running')
+
+    # Constantly looks for new comments and writes to queue
     while True:
         try:
             for r_comment in stream:
@@ -274,6 +353,7 @@ def stream_scraper_writer(q, stream, sub, parent_q):
                     time.sleep(3)
                 else:
                     q.append(r_comment)
+        # If stream fails, get new stream
         except PrawcoreException:
             stream = get_stream(sub)
         except Exception as e:
@@ -288,6 +368,7 @@ def stream_scraper_writer(q, stream, sub, parent_q):
 # /////////////////////////////////////////////////////////////////
 def queue_report(q, sub):
 
+    # sleeps for a minute then reports # of comments in queue
     while True:
         time.sleep(60)
         print('SSW ', _thread.get_native_id(), '\t| ', end='')
@@ -295,7 +376,7 @@ def queue_report(q, sub):
 
 # /////////////////////////////////////////////////////////////////
 #   Method: signal_handler
-#   Purpose: Handles escapes
+#   Purpose: Handles SIGINT (^C)
 # /////////////////////////////////////////////////////////////////
 def signal_handler(sig, frame):
     print('Abort detected.  Do you wish to quit?  Y/N')
@@ -331,18 +412,19 @@ def get_stream(sub):
 # /////////////////////////////////////////////////////////////////
 if __name__ == '__main__':
 
-
-
+    # subreddit instances
     wsb = reddit.subreddit('wallstreetbets')
     inv = reddit.subreddit('investing')
     sto = reddit.subreddit('stocks')
     pen = reddit.subreddit('pennystocks')
 
+    # stream instances
     wsb_stream = get_stream(wsb)
     inv_stream = get_stream(inv)
     sto_stream = get_stream(sto)
     pen_stream = get_stream(pen)
 
+    # stream comment queues
     wsb_comment_queue = []
     inv_comment_queue = []
     sto_comment_queue = []
@@ -350,6 +432,8 @@ if __name__ == '__main__':
 
 
     print('Starting threads')
+    # Starting threads for stream_scraper_writer, scrape_hot_posts, and stream_scraper_reader
+    # Each new sub needs one thread for each method
 
     t1 = _thread.start_new_thread(stream_scraper_writer, (wsb_comment_queue, wsb_stream, wsb, parent_q,))
     t2 = _thread.start_new_thread(stream_scraper_writer, (inv_comment_queue, inv_stream, inv, parent_q,))
@@ -366,7 +450,7 @@ if __name__ == '__main__':
     tA = _thread.start_new_thread(stream_scraper_reader, (sto_comment_queue, sto, parent_q,))
     tB = _thread.start_new_thread(stream_scraper_reader, (pen_comment_queue, pen, parent_q,))
 
-
+    # allow parent thread to idle
     idle(parent_q)
 
 
