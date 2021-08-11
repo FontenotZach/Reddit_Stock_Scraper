c32e88f6ced325ca310018a922090957e9298c9e
John Burns <john@burns.engineer>
2021-08-11 00:30:42 -0400

    Added already running check to run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 # Script to run the scraper within a screen
 
-screen python3 WSB_Scraper.py
+VAL=$(ps aux | grep 'WSB' | awk '$11 != "grep" {print}' | wc -l)
+
+if [ $VAL -eq 0 ]
+then
+    screen python3 WSB_Scraper.py
+else
+    echo "Could not start WSB_Scraper (Already running)"
+fi
