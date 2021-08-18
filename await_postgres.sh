f3330a18be9eec24cb75a4db382fab3d9527f9ee
John Burns <john@burns.engineer>
2021-08-18 14:17:48 -0400

    Added docker-compose functionality

---
+++ await_postgres.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+# Wait for postgres to start and begin accepting connections
+set -e
+
+until PGPASSWORD=$SQL_PASSWORD psql -h "$SQL_HOST" -d "$SQL_DB" -U "$SQL_USER" -c '\q'; do
+    >&2 echo "Postgres unavailable, sleeping"
+    sleep 1
+done
+
+>&2 echo "Postgres is up, starting.."
+exec "$@"
