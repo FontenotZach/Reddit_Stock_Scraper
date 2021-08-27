78065b70d9db7c5919fcd1acb1924f180dd6c530
John Burns <john@burns.engineer>
2021-08-26 22:21:13 -0400

    Fixed export script formatting.

--- set-exports.ps1
+++ set-exports.ps1
@@ -1,5 +1,5 @@
 # Exports the necessary secrets for the client.
 
-$env:SQL_PASSWORD=[A new password for your SQL Server]
-$env:PRAW_CLIENT_ID=[Your Reddit OAUTH Client ID]
-$env:PRAW_CLIENT_SECRET=[Your Reddit OAUTH Client Secret]
+$env:SQL_PASSWORD='A new password for your SQL Server'
+$env:PRAW_CLIENT_ID='Your Reddit OAUTH Client ID'
+$env:PRAW_CLIENT_SECRET='Your Reddit OAUTH Client Secret'
--- set-exports.sh
+++ set-exports.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # Exports the necessary secrets for the client.
 
-export SQL_PASSWORD=[A new password for your SQL Server]
-export PRAW_CLIENT_ID=[Your Reddit OAUTH Client ID]
-export PRAW_CLIENT_SECRET=[Your Reddit OAUTH Client Secret]
+export SQL_PASSWORD='A new password for your SQL Server'
+export PRAW_CLIENT_ID='Your Reddit OAUTH Client ID'
+export PRAW_CLIENT_SECRET='Your Reddit OAUTH Client Secret'
